c3b9aab7cf1f5cfe13286d8314c27160898dd9ac
Raul Souza <raul.3k@gmail.com>
2024-11-11 17:35:20 -0300

    Adjuste user route to retrieve current user

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Post, Body, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards, Request } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
 import { CreateUserDto } from '../dto/create-user.dto';
-import { ApiTags, ApiResponse, ApiParam } from '@nestjs/swagger';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
 
 @ApiTags('Users')
@@ -15,14 +15,13 @@ export class UsersController {
     return this.usersService.createUser(createUserDto);
   }
 
-  @Get(':id')
+  @Get()
   @UseGuards(JwtAuthGuard)
-  @ApiParam({ name: 'id', description: 'User ID', example: 'uuid-user-id' })
   @ApiResponse({
     status: 200,
     description: 'User details retrieved successfully.',
   })
-  async getById(@Param('id') userId: string) {
-    return this.usersService.getUserById(userId);
+  async getById(@Request() req: any) {
+    return this.usersService.getUserById(req.user.userId);
   }
 }
